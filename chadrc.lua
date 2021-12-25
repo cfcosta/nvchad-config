35c44aa6bb1f80c16bb47ebbad3dce0cb6998cd3
Cainã Costa <me@cfcosta.com>
2021-12-25 19:06:04 -0300

    Mapping and options adjustments

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,104 @@
 local M = {}
 
+M.plugins = {
+   -- enable/disable plugins (false for disable)
+   status = {
+      blankline = true, -- indentline stuff
+      bufferline = true, -- manage and preview opened buffers
+      colorizer = true, -- color RGB, HEX, CSS, NAME color codes
+      comment = true, -- easily (un)comment code, language aware
+      dashboard = false,
+      better_escape = true, -- map to <ESC> with no lag
+      feline = true, -- statusline
+      gitsigns = true,
+      lspsignature = true, -- lsp enhancements
+      telescope_media = false,
+      vim_matchup = true, -- improved matchit
+      cmp = true,
+      nvimtree = true,
+      autopairs = true,
+   },
+   options = {
+      autopairs = { loadAfter = "nvim-cmp" },
+      lspconfig = {
+         setup_lspconf = "", -- path of file containing setups of different lsps
+      },
+      nvimtree = {
+         enable_git = 0,
+
+         ui = {
+            allow_resize = true,
+            side = "left",
+            width = 25,
+            hide_root_folder = true,
+         },
+      },
+      luasnip = {
+         snippet_path = {},
+      },
+      statusline = {
+         -- hide, show on specific filetypes
+         hidden = {
+            "help",
+            "dashboard",
+            "NvimTree",
+            "terminal",
+         },
+         shown = {},
+
+         -- truncate statusline on small screens
+         shortline = true,
+         style = "default", -- default, round , slant , block , arrow
+      },
+      esc_insertmode_timeout = 300,
+   },
+   default_plugin_config_replace = {},
+}
+
+M.options = {
+   clipboard = "unnamedplus",
+   cmdheight = 1,
+   ruler = false,
+   hidden = true,
+   ignorecase = true,
+   smartcase = true,
+   mapleader = " ",
+   mouse = "a",
+   number = true,
+   numberwidth = 2,
+   relativenumber = false,
+   expandtab = true,
+   shiftwidth = 2,
+   smartindent = true,
+   tabstop = 8,
+   timeoutlen = 400,
+   updatetime = 250,
+   undofile = true,
+   fillchars = { eob = " " },
+
+   -- NvChad options
+   nvChad = {
+      copy_cut = true, -- copy cut text ( x key ), visual and normal mode
+      copy_del = true, -- copy deleted text ( dd key ), visual and normal mode
+      insert_nav = false, -- navigation in insertmode
+      window_nav = true,
+
+      -- updater
+      update_url = "https://github.com/NvChad/NvChad",
+      update_branch = "main",
+   },
+}
+
 M.mappings = {
   plugins = {
+   dashboard = {
+      bookmarks = "<leader>bm",
+      new_file = "<leader>fn", -- basically create a new buffer
+      open = "<leader>db", -- open dashboard
+      session_load = "<leader>l",
+      session_save = "<leader>s",
+   },
+
     bufferline = {
       next_buffer = "<leader>bn",
       prev_buffer = "leader>bp",
@@ -43,6 +140,26 @@ M.mappings = {
         media_files = "<leader>fp",
       },
     },
+
+   lspconfig = {
+      declaration = "gD",
+      definition = "gd",
+      hover = "K",
+      implementation = "gi",
+      signature_help = "gk",
+      add_workspace_folder = "<leader>wa",
+      remove_workspace_folder = "<leader>wr",
+      list_workspace_folders = "<leader>wl",
+      type_definition = "<leader>D",
+      rename = "<leader>rn",
+      code_action = "<leader>ca",
+      references = "gr",
+      float_diagnostics = "ge",
+      goto_prev = "[d",
+      goto_next = "]d",
+      set_loclist = "<leader>q",
+      formatting = "<leader>fm",
+   },
   }
 }
 
